0e8399f94bd4e1be31a45b86fef8c0cd700f2245
daenvil <julianlomba@hotmail.com>
2026-06-06 20:34:41 +0200

    sistema de puntuacion

--- src/exhibit.gd
+++ src/exhibit.gd
@@ -16,4 +16,29 @@ func _init(texture_rects: Array[TextureRect] = [], name_: String = "", id_: int 
 	id = id_
 
 func calculate_score() -> int:
-	return 100
+	var score := 0
+	var used_animals := {}
+	var used_terrains := {}
+	for texture: Texture2D in textures:
+		var file_name = texture.resource_path.get_file()
+		file_name = file_name.split(".")[0]
+		var name_parts = file_name.split("_")
+		var animal_name = name_parts[0]
+		used_animals[animal_name] = true
+		for name_part in name_parts:
+			if name_part in Global.tags_by_name_part:
+				for tag in Global.tags_by_name_part[name_part]:
+					if tag == Global.get_current_theme():
+						score += 100
+					if tag in [Global.Tag.OCEANO, Global.Tag.TIERRA, Global.Tag.AIRE]:
+						used_terrains[tag] = true
+	score += used_animals.size() * 100
+	score += used_terrains.size() * 100
+	score += mini(15, name.length())
+	if name.contains("a") and name.contains("e") and name.contains("i") and name.contains("o") and name.contains("u"):
+		score += 50
+	var used_letters := {}
+	for letter in name:
+		used_letters[letter] = true
+	score += used_letters.size() * 10
+	return score
--- src/global.gd
+++ src/global.gd
@@ -9,6 +9,38 @@ var scale_mode : bool = false
 var flip_mode : bool = false
 var drag_mode : bool = true
 
+enum Tag {
+	BOSQUE,
+	OCEANO,
+	PELIGROSA,
+	MASCOTA,
+	TIERRA,
+	AIRE,
+}
+
+var tags_by_name_part := {
+	"capybara": [Tag.MASCOTA, Tag.TIERRA],
+	"caracol": [Tag.MASCOTA, Tag.BOSQUE, Tag.TIERRA],
+	"escorpion": [Tag.PELIGROSA, Tag.TIERRA],
+	"gato": [Tag.BOSQUE, Tag.MASCOTA, Tag.PELIGROSA, Tag.TIERRA],
+	"pato": [Tag.OCEANO, Tag.MASCOTA, Tag.AIRE],
+	"pulpo": [Tag.OCEANO, Tag.PELIGROSA],
+	"tiburon": [Tag.OCEANO, Tag.PELIGROSA],
+	"tortuga": [Tag.OCEANO, Tag.MASCOTA, Tag.TIERRA],
+	"cuerno": [Tag.PELIGROSA],
+	"ciervo": [Tag.BOSQUE, Tag.TIERRA],
+	"vaca": [Tag.TIERRA],
+}
+
+var theme_by_day := {
+	1: Tag.BOSQUE,
+	2: Tag.PELIGROSA,
+	3: Tag.MASCOTA,
+}
+
+func get_current_theme() -> Tag:
+	return theme_by_day[Global.day]
+
 func _ready() -> void:
 	if !DirAccess.dir_exists_absolute("user://exhibits"):
 		DirAccess.make_dir_absolute("user://exhibits")
